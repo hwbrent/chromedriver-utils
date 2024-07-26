ed897ac1755f0caa49dfb4d957f4277a420a8c70
hwbrent <68196383+hwbrent@users.noreply.github.com>
2024-07-26 11:57:40 +0100

    Allow headless toggling

--- init.py
+++ init.py
@@ -2,14 +2,15 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 
 
-def init(chromedriver_path: str) -> webdriver.Chrome:
+def init(chromedriver_path: str, headless: bool = True) -> webdriver.Chrome:
     """
     Given the path of a `chromedriver` executable, this function initialises
     a `WebDriver`, and returns it
     """
 
     options = webdriver.ChromeOptions()
-    options.add_argument("--headless")  # Run Chrome in headless mode (no GUI)
+    if headless:
+        options.add_argument("--headless")  # Run Chrome in headless mode (no GUI)
     options.add_argument("--disable-gpu")  # Disable GPU acceleration in headless mode
 
     service = ChromeService(chromedriver_path)
